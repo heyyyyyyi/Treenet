99b678edcaa9af097e2c1ab4bd3fb70d1157bd2d
He Yi <147062688+heyyyyyyi@users.noreply.github.com>
2025-08-13 18:28:03 -0700

    config

--- adversarial_robustness_pytorch/core/utils/parser.py
+++ adversarial_robustness_pytorch/core/utils/parser.py
@@ -26,11 +26,11 @@ def parser_train():
                         help='Description of experiment. It will be used to name directories.')
 
     parser.add_argument('-m', '--model', choices=MODELS, default='wrn-28-10-swish', help='Model architecture to be used.')
-    parser.add_argument('--normalize', type=str2bool, default=False, help='Normalize input.')
+    parser.add_argument('--normalize', type=str2bool, default=True, help='Normalize input.')
     parser.add_argument('--pretrained-file', type=str, default=None, help='Pretrained weights file name.')
 
-    parser.add_argument('-na', '--num-adv-epochs', type=int, default=400, help='Number of adversarial training epochs.')
-    parser.add_argument('--adv-eval-freq', type=int, default=25, help='Adversarial evaluation frequency (in epochs).')
+    parser.add_argument('-na', '--num-adv-epochs', type=int, default=100, help='Number of adversarial training epochs.')
+    parser.add_argument('--adv-eval-freq', type=int, default=10, help='Adversarial evaluation frequency (in epochs).')
     
     parser.add_argument('--beta', default=None, type=float, help='Stability regularization, i.e., 1/lambda in TRADES.')
     
